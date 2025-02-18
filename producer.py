606a5c65270ae8dd1c3915b98f18c942d0a4db4b
soyeonkim <soyeonkim@foodtechkorea.com>
2025-02-18 14:15:03 +0900

    .

--- producer.py
+++ producer.py
@@ -10,7 +10,7 @@ channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
 
 # 패턴 2로만 메시지 전송
 routing_key = 'mp_mate-1001-1009782.instore-to'  # 패턴 1
-#routing_key = 'mp_mate-1001-1009782.instore.to.ssid002'  # 패턴 2
+# routing_key = 'mp_mate-1001-1009782.instore-to.ssid002'  # 패턴 2
 # routing_key = 'mp_mate-1001-1009782.instore-to.A01'  # 패턴 3
 messages = [
     {
